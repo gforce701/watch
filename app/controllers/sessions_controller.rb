276986fb8c930349e8d30dcb81ee03bc8e5be73b
gforce701 <56001837+gforce701@users.noreply.github.com>
2024-02-02 14:16:45 -0600

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by("LOWER(email) = ?", params[:session][:email].downcase)
+    user = User.find_by(email: params[:email])
     if user.nil?
       redirect_to unknown_user_path and return false
     end
@@ -17,27 +17,18 @@ class SessionsController < ApplicationController
 
   def token
     user = LoginToken.decode(token: params[:token])
+
     if user.present?
-      if user[:expired]
-        redirect_to expired_token_path(user[:user].id)
-      else
-        cookies.signed[:user_id] = { value: user.id, expires: 7.days.from_now, httponly: true }
-        redirect_to root_path
-      end
+      cookies.signed[:user_id] = { value: user.id, expires: 7.days.from_now, httponly: true }
+      redirect_to root_path
     else
       redirect_to unknown_user_path
     end
-  end
-
-  def unknown_user
-  end
-
-  def expired_token
-    @user = User.find(params[:user_id].to_i)
   end
 
   def destroy
     cookies.delete(:user_id)
     redirect_to root_path
   end
+
 end
